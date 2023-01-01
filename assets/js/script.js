4b7ac5ca045bb5d563af7ffcf4c6ad3da44fa688
Evandrosc <112897741+Evandrosc@users.noreply.github.com>
2023-01-01 19:20:41 -0300

    adicionado slider no main usando javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,3 +8,18 @@ function menuShow() {
         document.querySelector(".icon").src = "assets/img/close_white_36dp.svg";
     }
 }
+
+var slideIndex = 0;
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > x.length) {slideIndex = 1}
+  x[slideIndex-1].style.display = "block";
+  setTimeout(carousel, 7000); // Change image every 2 seconds
+}
