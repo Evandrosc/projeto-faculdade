d9f571747663e8b291075ea61fbd0a02dc1cf9cc
Evandrosc <112897741+Evandrosc@users.noreply.github.com>
2023-01-29 23:01:23 -0300

    melhorando html

---
+++ assets/js/institucional.js
@@ -0,0 +1,20 @@
+// menu mobile
+
+function menuShow() {
+    let menuMobile = document.querySelector(".mobile-menu");
+    if (menuMobile.classList.contains("open")) {
+        menuMobile.classList.remove("open");
+        document.querySelector(".icon").src = "assets/img/menu_white_36dp.svg";
+    }else {
+        menuMobile.classList.add("open");
+        document.querySelector(".icon").src = "assets/img/close_white_36dp.svg";
+    }
+}
+
+// sub menu dropwn mobile
+
+$(document).ready(function(){
+  $("#cursos-link").click(function(){
+    $("#sub-menu-cursos").toggleClass("active");
+  });
+});
