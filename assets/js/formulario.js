cd956cb894ee51d77e6e8a41c3f56194fb6f62db
Evandrosc <112897741+Evandrosc@users.noreply.github.com>
2023-02-19 20:46:35 -0300

    estrutura html e javascript melhorado

--- assets/js/formulario.js
+++ assets/js/formulario.js
@@ -1,13 +1,13 @@
 const selects = document.querySelectorAll('select');
 
 selects.forEach((select) => {
-  select.addEventListener('change', (event) => {
-    const selectedOption = event.target.value;
+    select.addEventListener('change', (event) => {
+        const selectedOption = event.target.value;
 
-    selects.forEach((otherSelect) => {
-      if (otherSelect !== select) {
-        otherSelect.disabled = selectedOption !== '';
-      }
+        selects.forEach((otherSelect) => {
+            if (otherSelect !== select) {
+                otherSelect.disabled = selectedOption !== '';
+            }
+        });
     });
-  });
 });
